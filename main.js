1622141242e5e74033ea61f0328fae5611e5b45c
thekhuong97 <thekhuong97@gmail.com>
2025-06-10 12:27:02 +0700

    pop2

--- main.js
+++ main.js
@@ -14,3 +14,19 @@ console.log(fruits.push2("Cherry")); //Trả về độ dài mới của mảng
 // console.log(fruits.push2("Orange", "Strawberry"));
 // console.log(fruits.push2("Mango", "Kiwi", "Watermelon"));
 console.log(fruits); //Mảng gốc bị thay đổi
+
+console.log(
+  "* Phương thức pop2: Loại bỏ phần tử cuối cùng của mảng và trả về phần tử đó. Mảng gốc bị thay đổi"
+);
+Array.prototype.pop2 = function () {
+  if (this.length === 0) {
+    return undefined;
+  }
+  let lastElement = this[this.length - 1];
+  this.length = this.length - 1;
+  return lastElement;
+};
+
+let colors = ["Red", "Green", "Blue"]; //Mảng gốc
+console.log(colors.pop2()); //Trả về phần tử bị xoá khỏi mảng
+console.log(colors); //Mảng gốc bị thay đổi
